96cb952723d4637a03ca0f7c4e3019286323b0e9
remyLarroye <38431872+remyLarroye@users.noreply.github.com>
2019-06-03 15:29:09 +0200

    Correction erreur du calcul de la note moyenne des stages d'un pays

--- back/app/utils/base-model.js
+++ back/app/utils/base-model.js
@@ -180,18 +180,17 @@ module.exports = class BaseModel {
       }
       else {
         Company.items.filter(company => company.countryId == query.countryId).forEach(company => {
-          average += this.getAverageRatingIntershipByCountryId({companyId: company.id});
+          average += parseInt(this.getAverageRatingIntershipByCountryId({companyId: company.id}), 10);
       })
         return average/nbIntership;
       }
     }
-
     if (query.companyId != null) {
       let buf = 0;
 
       this.items.filter(intership => intership.companyId == query.companyId).forEach(intership => {
         buf += intership.rating;
-      })
+    })
       return buf;
     }
   }
