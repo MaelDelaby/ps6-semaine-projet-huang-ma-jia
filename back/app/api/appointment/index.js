2363f22d710e10d7a3c0d8d70bd4b9e258bd1391
remyLarroye <38431872+remyLarroye@users.noreply.github.com>
2019-06-04 09:45:01 +0200

    Ajout de la possibilité de supprimé(delete) ou d ajouter(post) un appointment ou un availabilityTimeSlot

--- back/app/api/appointment/index.js
+++ back/app/api/appointment/index.js
@@ -4,6 +4,31 @@ const { Appointment } = require('../../models');
 const router = new Router();
 router.get('/receiverId', (req, res) => res.status(200).json(Appointment.getByReceiverId(req.query.receiverId)));
 router.get('/askerId', (req, res) => res.status(200).json(Appointment.getByAskerId(req.query.askerId)));
-router.get('/', (req, res) => res.status(200).json(Appointment.getAppointment()));
+
+router.post('/', (req, res) => {
+  try {
+    const appointment = Appointment.create(req.body);
+  res.status(201).json(appointment);
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      res.status(400).json(err.extra);
+    } else {
+      res.status(500).json(err);
+    }
+  }
+});
+
+router.delete('/:appointmentId', (req, res) => {
+  try {
+    Appointment.delete(req.params.appointmentId);
+  res.status(204).end();
+  } catch (err) {
+    if (err.name === 'NotFoundError') {
+      res.status(404).end();
+    } else {
+      res.status(500).json(err);
+    }
+  }
+});
 
 module.exports = router;
